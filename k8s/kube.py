3dbaa189e17dd65b774ba8160b9af5f02795f4c1
Thomas Tuminaro <thomastuminaro@MacBook-Air-de-Thomas.local>
2026-03-04 10:30:17 +0100

    started error mgmt, todo : unit testing + check error raising image not existing

--- k8s/kube.py
+++ k8s/kube.py
@@ -11,6 +11,7 @@ import json
 import time
 from pprint import pprint
 from typing import List, Dict, Optional
+import logging
 
 
 ########################################################################################################################
@@ -33,6 +34,10 @@ network = client.NetworkingV1Api()
 with open(RESOURCES_DETAILS_PATH, "r") as f:
     resources = json.load(f)
 
+logging.basicConfig(level=logging.INFO,
+                    filename="kube.log",
+                    filemode="a",
+                    format="%(asctime)s - %(levelname)s - %(message)s")
 
 ########################################################################################################################
 ########################################################################################################################
@@ -73,18 +78,31 @@ class Resource():
             func = identify_api(api_descr=self.kind, prefix="delete_namespaced_")
             try:
                 func(name=self.name, namespace=self.namespace) # type: ignore
-                return True
-            except:
-                print("Cannot delete resource for xyz.")
-                return False
+            except client.ApiException as err:
+                #print(type(manage_errors(err, self.name, self.kind, action="delete")[0]))
+                #print(manage_errors(err, self.name, self.kind, action="delete")[1])
+                if manage_errors(err, self.name, self.kind, action="delete")[0] == 404:
+                    msg = f"Skipping deletion of {self.kind} {self.name} : already deleted." #type: ignore
+                    logging.warning(msg)
+                    return
+                else:
+                    logging.error(manage_errors(err, self.name, self.kind, action="delete")[1])
+                    raise
+            else:
+                logging.info(f"Successfully deleted pod {self.name} from namespace {self.namespace}.")
+                return
+
         else:
             func = identify_api(api_descr=self.kind, prefix="delete_namespaced_")
             try:
                 func(name=self.name) # type: ignore
-                return True
-            except:
-                print("Cannot delete resource for xyz.")
-                return False 
+                return 
+            except client.ApiException as err:
+                manage_errors(err, self.name, self.kind, action="delete")
+                raise
+            except Exception as err:
+                print(f"Cannot delete resource due to {err}")
+                raise
 
 
 ########################################################################################################################
@@ -97,8 +115,7 @@ class Resource():
 class Pod(Resource):
     def __init__(self, name: str, namespace: str):
         super().__init__(name=name, namespace=namespace, kind="pod")
-
-       
+  
     def get_status(self):
         if super()._check_exists():
             pod_status = {
@@ -126,14 +143,14 @@ class Pod(Resource):
                 if pod.status.container_statuses[i].state.waiting.reason == "ImagePullBackOff" or pod.status.container_statuses[i].state.waiting.reason == "ErrImagePull": # type: ignore
                     print(f"Container image {pod.spec.containers[i].image} cannot be fetched.") # type: ignore
                     return False
-            print(f"Found container image {pod.spec.containers[i].image}") # type: ignore
+            #print(f"Found container image {pod.spec.containers[i].image}") # type: ignore
             i += 1
         return True
         
     def create(self, cont_image: str, labels: Optional[Dict] = None, storage: Optional[List[Dict]] = None):
         if not super()._check_exists():
             try: 
-                print(f"Creating pod {self.name} in namespace {self.namespace}...")
+                logging.info(f"Creating pod {self.name} in namespace {self.namespace}...")
                 pod_metadata = client.V1ObjectMeta(name=self.name, namespace=self.namespace, labels=labels)
                 pod_container = client.V1Container(name=self.name, image=cont_image)
                 containers = [pod_container]
@@ -143,19 +160,20 @@ class Pod(Resource):
 
                 time.sleep(5)
                 if not self._check_cont_status(containers):
-                    print("Failed to create the pod, image cannot be found, deleting pod...")
+                    logging.error(f"Failed to create the pod {self.name}, image cannot be found, deleting pod...")
                     super().delete_resource()
-                    return False
+                    raise
             except client.ApiException as err:
-                if "namespace" in err.body and "not found" in err.body: # type: ignore
-                    print(f"Cannot create pod as namespace {self.namespace} doesn't exist.")
-                    return False
+                err_status = manage_errors(err, self.name, self.kind, action="create")[0]
+                err_msg = manage_errors(err, self.name, self.kind, action="create")[1]
+                logging.error(err_msg)
+                raise
             else:
-                print("Succesfully created pod...")
-                return True
+                logging.info(f"Succesfully created pod {self.name} in namespace {self.namespace}")
+                return
         else:
-            print(f"The pod {self.name} already exist in namespace {self.namespace}, skipping...")
-            return False
+            logging.warning(f"The pod {self.name} already exist in namespace {self.namespace}, skipping...")
+            return
 
 
 ########################################################################################################################
@@ -251,6 +269,13 @@ def identify_api(api_descr: str, prefix: str):
             elif v["api"] == "NetworkingV1":
                 return getattr(network, f_name)
 
+def manage_errors(err : client.ApiException, rsc_name : str, rsc_kind : str, action: str):
+    try:
+        msg = f"Error with {action} of {rsc_kind} {rsc_name} : {json.loads(err.body).get("message", err.reason).replace('"', '')}" #type: ignore
+    except Exception as e:
+        msg = f"Error with {action} of {rsc_kind} {rsc_name} : unknown error." #type: ignore
+ 
+    return (err.status, msg)
 
 ########################################################################################################################
 ########################################################################################################################
@@ -260,22 +285,18 @@ def identify_api(api_descr: str, prefix: str):
 
 
 if __name__ == "__main__":
-    a = Resource(name="view", kind="clusterrole")
-    b = Resource(name="nginx", kind="deploy", namespace="default")
-    c = Pod(name="csi-node-driver-62whl", namespace="calico-system")
-    d = Deploy(name="testdeploy", namespace="default")
+    a = Pod(name="test1", namespace="default")
+    a.create(cont_image="nginx")
 
-    common_labels = {
-        "project": "python",
-        "app-type": "web"
-    }
-    #print(a._check_exists()) # type: ignore
+    b = Pod(name="test1", namespace="default")
+    b.create(cont_image="nginx")
 
-    #c.create(cont_image="nginx", labels=common_labels)
-    d.create(image="nginx", deploy_replicas=2)
-    time.sleep(10)
-    print(d.get_status())
-    #print(len(c.get_resource().spec.containers))
+    a.delete_resource()
+    time.sleep(5)
+    a.delete_resource()
 
-    #b.delete_resource()
+    c = Pod(name="test2", namespace="default")
+    c.create(cont_image="ngifezfeznx")
+   
+
 
